202443eee46d003ae2a2a6b914dcedb05b7d3c33
CHEN BOON KIT <k03963369@gmail.com>
2026-05-29 16:17:15 +0800

    Update transaction.sv

--- tb/transaction.sv
+++ tb/transaction.sv
@@ -1,1 +1,31 @@
+class transaction #(parameter DATA_WIDTH = 8);
+    // Rand variables for Constrained Random Verification (CRV)
+    rand bit                  wr_en;
+    rand bit                  rd_en;
+    rand bit [DATA_WIDTH-1:0] data_in;
+         bit [DATA_WIDTH-1:0] data_out;
+         bit                  full;
+         bit                  empty;
 
+    // Distribution constraint to bias stimulus towards high-density activity
+    constraint rw_dist {
+        wr_en dist {1 := 50, 0 := 50};
+        rd_en dist {1 := 40, 0 := 60};
+    }
+
+    // Copy function for reference model operations
+    function transaction copy();
+        copy = new();
+        copy.wr_en    = this.wr_en;
+        copy.rd_en    = this.rd_en;
+        copy.data_in  = this.data_in;
+        copy.data_out = this.data_out;
+        copy.full     = this.full;
+        copy.empty    = this.empty;
+    endfunction
+
+    function void display(string name);
+        $display("[%s] WR:%b RD:%b | DataIn:0x%0h | DataOut:0x%0h | Full:%b Empty:%b", 
+                 name, wr_en, rd_en, data_in, data_out, full, empty);
+    endfunction
+endclass
